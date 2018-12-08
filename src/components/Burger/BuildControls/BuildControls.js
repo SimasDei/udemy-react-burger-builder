b5f3811f252ed481f49df66eb8c0f1a6cb35e709
SimasDei <simondeiman@gmail.com>
2018-12-08 15:24:09 +0200

    add dynamic total price display

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,11 +12,14 @@ const controls = [
 const BuildControls = (props) => {
   return (
     <div className={classes.BuildControls}>
+      <p>Current Price: $<strong>{props.price}</strong></p>
       {controls.map(ctrl =>(
         <BuildControl
           key={ctrl.label}
           label={ctrl.label}
-          added={() => props.ingredientAdded(ctrl.type)}/>
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]} />
       ))}
     </div>
   )
